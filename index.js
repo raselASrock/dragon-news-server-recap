95df33c3cf54a5765dc3df27206a10e5fc20a4c9
raSel <mdraselm413@gmail.com>
2022-10-21 23:01:09 +0600

    final

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ app.use(cors())
 
 
 const categories = require('./data/categories.json');
+const news = require('./data/news.json');
 
 
 app.get("/", (req, res) =>{
@@ -18,6 +19,30 @@ app.get("/", (req, res) =>{
 app.get("/news-categories", (req, res) =>{
     res.send(categories);
 })
+
+app.get("/news", (req, res) =>{
+    res.send(news)
+})
+
+app.get("/news/:id", (req, res) =>{
+    const id = req.params.id;
+    const selectedNews = news.find(n => n._id === id)
+    res.send(selectedNews)
+})
+
+
+app.get("/category/:id", (req, res) =>{
+    const id = req.params.id;
+    if(id === "08"){
+        res.send(news);
+    }
+    else{
+        const category_news = news.filter(n => n.category_id === id)
+    res.send(category_news)
+    }
+    
+})
+
 app.listen(port, () =>{
     console.log("Dragon News Server Running on port: ", port);
 })
